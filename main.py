37e800923f973bec033ccac2421e2798bf1ce01c
saubuny <saubuny@gmail.com>
2024-05-07 15:39:22 -0400

    draw lines and make window float

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from tkinter import Tk, BOTH, Canvas
+from tkinter import Tk, Canvas
+
+
+class Point:
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point) -> None:
+        self.__p1 = p1
+        self.__p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color: str) -> None:
+        canvas.create_line(
+            self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2
+        )
 
 
 class Window:
-    def __init__(self, width, height) -> None:
+    def __init__(self, width: int, height: int) -> None:
         self.__root = Tk(className="Maze")
         self.__root.title("Maze")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.attributes("-type", "dialog")
         self.__canvas = Canvas(width=width, height=height)
         self.__canvas.pack()
         self.__running = False
@@ -22,9 +40,14 @@ class Window:
     def close(self) -> None:
         self.__running = False
 
+    def draw_line(self, line: Line, fill_color: str) -> None:
+        line.draw(self.__canvas, fill_color)
+
 
 def main() -> None:
     win = Window(800, 800)
+    win.draw_line(Line(Point(0, 100), Point(400, 300)), "black")
+    win.draw_line(Line(Point(443, 700), Point(546, 654)), "red")
     win.wait_for_close()
 
 
