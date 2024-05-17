36316c1da294085a291dfff7b4fe04f550a75e48
saubuny <saubuny@gmail.com>
2024-05-17 02:37:54 -0400

    generate maze

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import Tk, Canvas
+from typing import Self
+from time import sleep
+import random
 
 
 class Point:
@@ -23,8 +26,8 @@ class Window:
         self.__root = Tk(className="Maze")
         self.__root.title("Maze")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__root.attributes("-type", "dialog")
-        self.__canvas = Canvas(width=width, height=height)
+        self.__root.attributes("-type", "dialog")  # Floating window
+        self.__canvas = Canvas(width=width, height=height, background="white")
         self.__canvas.pack()
         self.__running = False
 
@@ -45,45 +48,164 @@ class Window:
 
 
 class Cell:
-    def __init__(self, win: Window) -> None:
+    def __init__(self, win: Window | None = None) -> None:
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
 
-        self._x1 = None
-        self._x2 = None
-        self._y1 = None
-        self._y2 = None
+        self.x1 = 0
+        self.x2 = 0
+        self.y1 = 0
+        self.y2 = 0
         self._win = win
 
-    def draw(self, top_left: tuple[int, int], bottom_right: tuple[int, int]) -> None:
-        self._x1 = top_left[0]
-        self._y1 = top_left[1]
-        self._x2 = bottom_right[0]
-        self._y2 = bottom_right[1]
+        self.visited = False
 
-        if self.has_left_wall:
-            left = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
-            self._win.draw_line(left, "black")
-        if self.has_right_wall:
-            right = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
-            self._win.draw_line(right, "black")
-        if self.has_top_wall:
-            top = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
-            self._win.draw_line(top, "black")
-        if self.has_bottom_wall:
-            bottom = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-            self._win.draw_line(bottom, "black")
+    def draw(self, top_left: tuple[int, int], bottom_right: tuple[int, int]) -> None:
+        self.x1 = top_left[0]
+        self.y1 = top_left[1]
+        self.x2 = bottom_right[0]
+        self.y2 = bottom_right[1]
+
+        if self._win:
+            left = Line(Point(self.x1, self.y1), Point(self.x1, self.y2))
+            right = Line(Point(self.x2, self.y1), Point(self.x2, self.y2))
+            top = Line(Point(self.x1, self.y1), Point(self.x2, self.y1))
+            bottom = Line(Point(self.x1, self.y2), Point(self.x2, self.y2))
+            self._win.draw_line(left, "black" if self.has_left_wall else "white")
+            self._win.draw_line(right, "black" if self.has_right_wall else "white")
+            self._win.draw_line(top, "black" if self.has_top_wall else "white")
+            self._win.draw_line(bottom, "black" if self.has_bottom_wall else "white")
+
+    def draw_move(self, to_cell: Self, undo: bool = False) -> None:
+        color = "gray" if undo else "red"
+        self_center = ((self.x1 + self.x2) // 2, (self.y1 + self.y2) // 2)
+        other_center = ((to_cell.x1 + to_cell.x2) // 2, (to_cell.y1 + to_cell.y2) // 2)
+        line = Line(
+            Point(self_center[0], self_center[1]),
+            Point(other_center[0], other_center[1]),
+        )
+        if self._win:
+            self._win.draw_line(line, color)
+
+
+class Maze:
+    def __init__(
+        self,
+        x1: int,
+        y1: int,
+        num_rows: int,
+        num_cols: int,
+        cell_size_x: int,
+        cell_size_y: int,
+        win: Window | None = None,
+        seed: int | None = None,
+    ) -> None:
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        self._reset_cells_visited()
+        if seed is not None:
+            random.seed(seed)
+
+    def _create_cells(self) -> None:
+        self._cells = [
+            [Cell(self._win) for _ in range(self._num_rows)]
+            for _ in range(self._num_cols)
+        ]
+
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(self._cells, i, j)
+
+    def _draw_cell(self, cells: list[list[Cell]], i: int, j: int) -> None:
+        x = i * self._cell_size_x + self._x1
+        y = j * self._cell_size_y + self._y1
+        if self._win:
+            cells[i][j].draw((x, y), (x + self._cell_size_x, y + self._cell_size_y))
+        self._animate()
+
+    def _animate(self) -> None:
+        if self._win:
+            self._win.redraw()
+            sleep(0.005)
+
+    def _break_entrance_and_exit(self) -> None:
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(self._cells, 0, 0)
+        self._cells[-1][-1].has_bottom_wall = False
+        self._draw_cell(self._cells, self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i: int, j: int) -> None:
+        self._cells[i][j].visited = True
+        while True:
+            to_visit: list[tuple[int, int]] = []
+            # Manually check all neighbors
+            if self._can_visit_cell(i - 1, j):
+                to_visit.append((i - 1, j))
+            if self._can_visit_cell(i + 1, j):
+                to_visit.append((i + 1, j))
+            if self._can_visit_cell(i, j + 1):
+                to_visit.append((i, j + 1))
+            if self._can_visit_cell(i, j - 1):
+                to_visit.append((i, j - 1))
+
+            if len(to_visit) == 0:
+                self._draw_cell(self._cells, i, j)
+                return
+
+            direction = random.choice(to_visit)
+
+            if direction[0] == i and direction[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[direction[0]][direction[1]].has_top_wall = False
+                self._draw_cell(self._cells, i, j)
+                self._draw_cell(self._cells, direction[0], direction[1])
+            elif direction[0] == i and direction[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[direction[0]][direction[1]].has_bottom_wall = False
+                self._draw_cell(self._cells, i, j)
+                self._draw_cell(self._cells, direction[0], direction[1])
+            elif direction[0] == i + 1 and direction[1] == j:
+                self._cells[i][j].has_right_wall = False
+                self._cells[direction[0]][direction[1]].has_left_wall = False
+                self._draw_cell(self._cells, i, j)
+                self._draw_cell(self._cells, direction[0], direction[1])
+            elif direction[0] == i - 1 and direction[1] == j:
+                self._cells[i][j].has_left_wall = False
+                self._cells[direction[0]][direction[1]].has_right_wall = False
+                self._draw_cell(self._cells, i, j)
+                self._draw_cell(self._cells, direction[0], direction[1])
+
+            self._break_walls_r(direction[0], direction[1])
+
+    def _reset_cells_visited(self) -> None:
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
+
+    # Safe indexing, there is also a better way to do this, i am just lazy and do not care about this guided project
+    # I will try harder on the next one, i just have a thing against cell-based projects...
+    def _can_visit_cell(self, i: int, j: int) -> bool:
+        if i < 0 or j < 0:
+            return False
+        try:
+            return not self._cells[i][j].visited
+        except IndexError:
+            return False
 
 
 def main() -> None:
     win = Window(800, 800)
-    c1 = Cell(win)
-    c2 = Cell(win)
-    c1.has_bottom_wall = False
-    c1.draw((400, 400), (500, 500))
-    c2.draw((600, 400), (700, 500))
+    Maze(8, 8, 20, 20, 39, 39, win, 0)
     win.wait_for_close()
 
 
---
+++ tests.py
@@ -0,0 +1,24 @@
+import unittest
+from main import Maze
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_break_walls(self):
+        print("i really do not want to write tests right now, i am sorry")
+
+
+if __name__ == "__main__":
+    unittest.main()
